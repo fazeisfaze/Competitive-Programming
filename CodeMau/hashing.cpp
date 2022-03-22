caaa504fadc33b0702cbd32e4e9a0b77e74af106
Huỳnh Thanh Phúc <phucbin0903@gmail.com>
2022-03-22 13:24:56 +0700

    new changes

--- CodeMau/hashing.cpp
+++ CodeMau/hashing.cpp
@@ -23,22 +23,12 @@ int dY[]= {-1,0,1,0};
 const ll oo=1e18;
 const ll MOD=1e9+7;
 const ll mod=1e9+9;
-bool isPrime(ll n){
-
-    for (ll i = 2;i*i<=n;i++)
-        if (n%i==0)
-         return 0;
-   return 1;
-}
-void solve(){
-    ll n;
-    cin>>n;
-     if (n==1) cout<<0<<endl;
-     else{
-         if (isPrime(n*n+n*(n-1)+(n-1)*(n-1)))
-            cout<<1<<endl;
-         else cout<<0<<endl;;
-     }
+const int LIM = 1e6+1;
+const int base = 31;
+ll Pow[LIM],hashA[LIM];
+string a,b;
+ll getHash(int i, int j){
+    return (hashA[j]-hashA[i-1]*Pow[j-i+1]+mod*mod)%mod;
 
 }
 int main()
@@ -46,9 +36,20 @@ int main()
     fast;
     //freopen(".INP","r",stdin);
     //freopen(".OUT","w",stdout);
-    int t;
-    cin>>t;
-    while(t--) solve();
+    cin>>a>>b;
+    ll hashB=0;
+    FORW(i,0,b.size()-1)
+        hashB=(hashB *base+ b[i]-'a'+1)%mod;
+    Pow[0]=1;
+    hashA[0]=0;
+    FORW(i,1,a.size()){
+        Pow[i]=(Pow[i-1]*base)%mod;
+        hashA[i]=(hashA[i-1]*base+a[i-1]-'a'+1)%mod;
+    }
+    FORW(i,0,a.size()-b.size())
+     if (hashB==getHash(i+1,i+b.size()))
+        cout<<i+1<<" ";
+
 
 }
 
